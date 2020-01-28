8728a067202cbb1a5038a736fc5405eb66cf0eac
Aaron <github@otacon.press>
2020-01-27 21:12:38 -0500

    Add EventNames enum

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,13 @@ export enum Registry {
     CurrentColor = 'currentOrbColor',
     Score = 'score',
 }
+
+export enum EventNames {
+    ColorChange = 'colorChange',
+    PlayerHit = 'playerHit',
+    RegeneratingStamina = 'regeneratingStamina',
+    StaminaMaxed = 'staminaMaxed',
+    ReduceHealth = 'reduceHealth',
+    IncreaseHealth = 'increaseHealth',
+    ScoreIncrease = 'scoreIncrease',
+}
