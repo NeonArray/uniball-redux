ca09bdabdfd685b06ee41f9972e34ad9dba22bac
Aaron <github@otacon.press>
2020-01-27 21:17:04 -0500

    Remove HUD and Debug scene from stack

--- src/scenes/scenes.ts
+++ src/scenes/scenes.ts
@@ -1,8 +1,8 @@
 
 import main from './main';
-import debug from '../components/debug';
-import hud from './hud';
+// import debug from '../components/debug';
+// import hud from './hud';
 
 export default [
-    main, debug, hud
+    main
 ];
