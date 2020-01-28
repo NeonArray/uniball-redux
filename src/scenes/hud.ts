33c91103ab1dfadaa2ac0b7b91646526b29f15fb
Aaron <github@otacon.press>
2020-01-27 21:17:34 -0500

    Comment out code

--- src/scenes/hud.ts
+++ src/scenes/hud.ts
@@ -1,4 +1,5 @@
 import 'phaser';
+import {EventNames} from "../types";
 
 export default class HUD extends Phaser.Scene {
     private staminaGauge: integer = 7;
@@ -11,26 +12,28 @@ export default class HUD extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.multiatlas('s_hud', 'objects/objects.json', 'objects');
+        // const a = this.cache.json.get('s_objects');
+        //
+        // this.load.multiatlas('s_hud', 'objects/objects.json', 'objects');
     }
 
     create(): void {
-        this.anims.create({
-            key: 'stamina',
-            frames: this.anims.generateFrameNames('s_hud', {
-                start: 1, end: 11,
-                suffix: '.png',
-            }),
-            frameRate: 0,
-            repeat: 0,
-        });
-
-        const bar = this.add.sprite(100, this.game.canvas.height - 10, 's_hud', 'BombBar/4.png');
-        bar.setScale(4,4);
-
-        this.scene.get('MainScene').events.on('regenerateStamina', (amount) => {
-            bar.setFrame(`BombBar/${amount}.png`);
-        }, this);
+        // this.anims.create({
+        //     key: 'stamina',
+        //     frames: this.anims.generateFrameNames('s_hud', {
+        //         start: 1, end: 11,
+        //         suffix: '.png',
+        //     }),
+        //     frameRate: 0,
+        //     repeat: 0,
+        // });
+        //
+        // const bar = this.add.sprite(100, this.game.canvas.height - 10, 's_hud', 'BombBar/4.png');
+        // bar.setScale(4,4);
+        //
+        // this.scene.get('MainScene').events.on(EventNames.RegeneratingStamina, (amount) => {
+        //     bar.setFrame(`BombBar/${amount}.png`);
+        // }, this);
     }
 
     update(): void {}
