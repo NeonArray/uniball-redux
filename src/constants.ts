68d0400a7af61cd1da32541f14cc1bd293efe91b
Aaron <github@otacon.press>
2020-01-27 21:16:48 -0500

    Modify constant values

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
 
 export enum Constants {
-    P_MOVE_SPEED = 200,
-    P_JUMP_HEIGHT = 185,
+    P_MOVE_SPEED = 250,
+    P_JUMP_HEIGHT = 190,
     P_SPIKE_SPEED = 300,
+    P_MAX_STAMINA = 6,
+    P_MAX_HEALTH = 3,
+    SHEET_KEY = 's_sheet',
 }
